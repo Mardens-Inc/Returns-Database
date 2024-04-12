e2d2a05eba1df17d62ddba360f7749956e5cd642
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-12 15:37:57 -0400

    Update class usage in post.php
    The class 'Employees' in the post.php file of the API is changed to 'Returns'. This will affect the instantiation of the $employees object, which will now create an instance of the 'Returns' class.

--- api/post.php
+++ api/post.php
@@ -1,6 +1,6 @@
 <?php
 
-use EmployeeList\Employees;
+use EmployeeList\Returns;
 use Slim\Factory\AppFactory;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
@@ -10,7 +10,7 @@ $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 $app->setBasePath("/api");
 require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/php/Employees.php';
-$employees = new Employees();
+$employees = new Returns();
 
 
 $app->post("/import", function ($request, $response, $args) use ($employees) {
