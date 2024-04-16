2818435644f27b4f921833ecbffbae9f5cb9c73b
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-16 13:32:31 -0400

    Rename connections.inc.php to Connection.php and add namespaces
    Renamed the 'connections.inc.php' file to 'Connection.php' for better naming consistency. Introduced the 'ReturnsDatabase' namespace along with 'mysqli' and 'mysqli_sql_exception' use declarations to handle SQL exceptions better.

--- assets/php/Connection.php
+++ assets/php/Connection.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace ReturnsDatabase;
+use mysqli;
+use mysqli_sql_exception;
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
