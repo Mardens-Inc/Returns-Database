6e74a38950a02a849ffd3fdd428aeb3be21691ac
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-16 13:32:20 -0400

    Simplify UUID generation in config.inc.php
    The code to generate the UUID has been refactored. The process is now more streamlined, directly returning the generated UUID without the use of redundant intermediate variables. This reduces clutter and improves readability.

--- assets/php/config.inc.php
+++ assets/php/config.inc.php
@@ -39,12 +39,10 @@ function getGUID()
     } else {
         mt_srand((float)microtime() * 10000); //optional for php 4.2.0 and up.
         $charid = strtoupper(md5(uniqid(rand(), true)));
-        $uuid =
-            substr($charid, 0, 8)
-            . substr($charid, 8, 4)
-            . substr($charid, 12, 4)
-            . substr($charid, 16, 4)
-            . substr($charid, 20, 12);
-        return $uuid;
+        return substr($charid, 0, 8)
+        . substr($charid, 8, 4)
+        . substr($charid, 12, 4)
+        . substr($charid, 16, 4)
+        . substr($charid, 20, 12);
     }
 }
