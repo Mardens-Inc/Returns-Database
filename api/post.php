d02485d418f74546cf27c3aa8b96eb6cb3aba13c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-16 13:31:58 -0400

    Refactor API to use ReturnItem instead of Employees
    The code has been modified to replace the existing EmployeeList Returns with the ReturnsDatabase ReturnItem. This also refactors the endpoint to handle inserting a return item into the database, replacing the import functionality, and provides improved error handling.

--- api/post.php
+++ api/post.php
@@ -1,6 +1,6 @@
 <?php
 
-use EmployeeList\Returns;
+use ReturnsDatabase\ReturnItem;
 use Slim\Factory\AppFactory;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
@@ -9,14 +9,20 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 $app->setBasePath("/api");
-require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/php/Employees.php';
-$employees = new Returns();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/php/ReturnItem.php';
 
 
-$app->post("/import", function ($request, $response, $args) use ($employees) {
-    $file = $_FILES["file"] ?? [];
-    $contentType = $request->getHeader("Content-Type")[0];
-    return $response->withHeader("Content-Type", "application/json")->withJson(@$employees->import($contentType, $file));
+$app->post("/", function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    $returnItem = ReturnItem::fromJson($data);
+    try {
+        if ($returnItem->insert()) {
+            return $response->withStatus(200)->withJson($returnItem);
+        } else
+            return $response->withStatus(500)->withJson(["error" => "Failed to insert"]);
+    } catch (Exception $e) {
+        return $response->withStatus(500)->withJson(["error" => $e->getMessage()]);
+    }
 });
 
 $app->run();
