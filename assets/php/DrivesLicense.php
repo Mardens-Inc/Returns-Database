b5238f5683907af49797cfabfcc3bddf514231c8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-17 08:47:11 -0400

    Add more fields and methods to DrivesLicense class
    The `DrivesLicense` class was updated to include additional fields (issued_date, expiration_date, height, eye_color) along with their extraction. Methods for gender determination and height calculation were also added. An encodeBase64 method was introduced too. Some code was refactored, replacing filterAlphaNumeric method with direct regular expression usage.

--- assets/php/DrivesLicense.php
+++ assets/php/DrivesLicense.php
@@ -2,6 +2,10 @@
 
 namespace ReturnsDatabase;
 
+use DateInterval;
+use DateTime;
+use Exception;
+
 class DrivesLicense
 {
     public string $code;
@@ -16,6 +20,10 @@ class DrivesLicense
     public string $zip;
     public string $date_of_birth;
     public string $gender;
+    public string $issued_date;
+    public string $expiration_date;
+    public string $height;
+    public string $eye_color;
 
     public function __construct(string $code)
     {
@@ -28,17 +36,61 @@ class DrivesLicense
         $this->address = self::extractString($code, "DAG", "DAI");
         $this->city = self::extractString($code, "DAI", "DAJ");
         $this->state = self::extractString($code, "DAJ", "DAK");
-        $this->zip = self::extractString($code, "DAK", "DCF");
+        $this->zip = substr(self::extractString($code, "DAK", "DCF"), 0, 5);
         $this->date_of_birth = self::extractDate(self::extractString($code, "DBB", "DBA"));
+        $this->issued_date = self::extractDate(self::extractString($code, "DBD", "DBB"));
+        $this->expiration_date = self::extractDate(self::extractString($code, "DBA", "DBC"));
+        $this->eye_color = self::extractString($code, "DAY", "DAG");
         $this->gender = self::determineGender(self::extractString($code, "DBC", "DAU"));
+        $this->height = $this->calculateHeight(self::extractString($code, "DAU", "DAY"));
     }
 
-    private function determineGender($code)
+    /**
+     * Determine the gender based on the given code.
+     *
+     * @param string $code The code representing the gender.
+     *
+     * @return string The gender as a string.
+     */
+    private function determineGender(string $code): string
     {
-        return $code === "1" ? "M" : ($code === "2" ? "F" : "");
+        $genderCode = intval($code);
+        if ($genderCode == 1) {
+            return "Male";
+        }
+        if ($genderCode == 2) {
+            return "Female";
+        }
+        if ($genderCode == 9) {
+            return "Other";
+        }
+        return "Not Specified";
     }
 
-    private function extractDate($input)
+    private function calculateHeight(string $code): string
+    {
+        if (str_ends_with($code, "IN")) {
+            $heightCode = intval(substr($code, 0, -3));
+            $feet = floor($heightCode / 12);
+            $inches = $heightCode % 12;
+            return $feet . "ft " . $inches . "in";
+        }
+        if (str_ends_with($code, "CM")) {
+            $heightCode = intval(substr($code, 0, -3));
+            $feet = floor($heightCode / 30.48);
+            $inches = round(($heightCode % 30.48) / 2.54);
+            return $feet . "ft " . $inches . "in";
+        }
+        return "Not Specified";
+    }
+
+    /**
+     * Extracts the date from the input string and returns it in the format Y-m-d.
+     *
+     * @param string $input The input string containing the date.
+     * @return string The extracted date in the format Y-m-d.
+     */
+    private function extractDate(string $input): string
     {
         $month = intval(substr($input, 0, 2));
         $day = intval(substr($input, 2, 2));
@@ -46,17 +98,20 @@ class DrivesLicense
         return date("Y-m-d", mktime(0, 0, 0, $month, $day, $year));
     }
 
-    private function extractString($input, $startMarker, $endMarker)
+    /**
+     * Extracts a string from the given input string between the start marker and end marker.
+     *
+     * @param string $input The input string to extract the substring from.
+     * @param string $startMarker The start marker for the substring.
+     * @param string $endMarker The end marker for the substring.
+     * @return string The extracted substring, or an empty string if the markers are not found.
+     */
+    private function extractString(string $input, string $startMarker, string $endMarker): string
     {
         $startIndex = strpos($input, $startMarker);
         $endIndex = strpos($input, $endMarker, $startIndex + strlen($startMarker));
         if ($startIndex === false || $endIndex === false) return "";
         return substr($input, $startIndex + strlen($startMarker), $endIndex - $startIndex - strlen($startMarker));
-    }
-
-    private function filterAlphaNumeric($input)
-    {
-        return preg_replace("/[^a-zA-Z0-9]/", '', $input);
     }
 
     /**
@@ -69,6 +124,11 @@ class DrivesLicense
         $arr = (array)$this;
         unset($arr["code"]);
         return $arr;
+    }
+
+    private function encodeBase64(string $input): string
+    {
+        return base64_encode($input);
     }
 
 }
