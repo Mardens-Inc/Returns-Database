0da7ee61bb09079929a1cc4d6c0192ab21ec7828
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-22 16:23:08 -0400

    Add new entries to the stores database
    This commit inserts new store locations including city and address to the stores database. It helps to keep the database updated with the latest store locations across various cities.

---
+++ stores.sql
@@ -0,0 +1,14 @@
+insert into stores (city, address) values ('Madawaska','81 Fox St');
+insert into stores (city, address) values ('Presque Isle','803 Main St');
+insert into stores (city, address) values ('Houlton','120 North St');
+insert into stores (city, address) values ('Lincoln','28 Main St');
+insert into stores (city, address) values ('Calais','189 Main St');
+insert into stores (city, address) values ('Brewer','564 Wilson St');
+insert into stores (city, address) values ('Waterville','458 Kennedy Memorial Dr');
+insert into stores (city, address) values ('Ellsworth','33 Mardens Way Suite A');
+insert into stores (city, address) values ('Lewiston','750 Main St');
+insert into stores (city, address) values ('Gray','65 W Gray Rd');
+insert into stores (city, address) values ('Scarborough','451 Payne Rd');
+insert into stores (city, address) values ('Biddeford','435 Elm St');
+insert into stores (city, address) values ('Sanford','1247 Main St');
+
